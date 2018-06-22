100fa2dd1097fa95f7197c12256d61f18bcfee3a
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-06-22 15:12:24 +0300

    updated packages

--- src/Lykke.Job.Balances/Controllers/IsAliveController.cs
+++ src/Lykke.Job.Balances/Controllers/IsAliveController.cs
@@ -1,7 +1,5 @@
-﻿using System.Linq;
-using System.Net;
+﻿using System.Net;
 using Lykke.Common.Api.Contract.Responses;
-using Lykke.Service.Balances.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -11,49 +9,26 @@ namespace Lykke.Job.Balances.Controllers
     [Route("api/[controller]")]
     public class IsAliveController : Controller
     {
-        private readonly IHealthService _healthService;
-
-        public IsAliveController(IHealthService healthService)
-        {
-            _healthService = healthService;
-        }
-
         /// <summary>
         /// Checks service is alive
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         [SwaggerOperation("IsAlive")]
-        [ProducesResponseType(typeof(IsAliveResponse), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(IsAliveResponse), (int)HttpStatusCode.OK)]        
         public IActionResult Get()
         {
-            var healthViloationMessage = _healthService.GetHealthViolationMessage();
-            if (healthViloationMessage != null)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, new ErrorResponse
-                {
-                    ErrorMessage = $"Job is unhealthy: {healthViloationMessage}"
-                });
-            }
-
             // NOTE: Feel free to extend IsAliveResponse, to display job-specific indicators
             return Ok(new IsAliveResponse
             {
                 Name = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationName,
                 Version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion,
                 Env = Program.EnvInfo,
-//#$if DEBUG
+#if DEBUG
                 IsDebug = true,
-//#$else
-                //$#$//IsDebug = false,
-//#$endif
-                IssueIndicators = _healthService.GetHealthIssues()
-                    .Select(i => new IsAliveResponse.IssueIndicator
-                    {
-                        Type = i.Type,
-                        Value = i.Value
-                    })
+#else
+                IsDebug = false,
+#endif
             });
         }
     }
