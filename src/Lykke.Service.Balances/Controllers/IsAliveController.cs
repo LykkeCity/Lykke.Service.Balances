32e7351592f73f26e4fda9c1d7fbee0df1d12e65
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-06-27 09:44:55 +0300

    changed IsAlive for balances service

--- src/Lykke.Service.Balances/Controllers/IsAliveController.cs
+++ src/Lykke.Service.Balances/Controllers/IsAliveController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Net;
 using Lykke.Common.Api.Contract.Responses;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -19,22 +18,17 @@ namespace Lykke.Service.Balances.Controllers
         [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
         public IActionResult Get()
         {
-            try
+            return Ok(new IsAliveResponse
             {
-                var isAliveResponse = new IsAliveResponse
-                {
-                    Version =
-                        Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application
-                            .ApplicationVersion,
-                    Env = Environment.GetEnvironmentVariable("Env")
-                };
-
-                return Ok(isAliveResponse);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(ErrorResponse.Create(e.Message));
-            }
+                Name = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationName,
+                Version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion,
+                Env = Program.EnvInfo,
+#if DEBUG
+                IsDebug = true,
+#else
+                IsDebug = false,
+#endif
+            });
         }
     }
 }
--- src/Lykke.Service.Balances/Program.cs
+++ src/Lykke.Service.Balances/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Lykke.Service.Balances
 {
-    public class Program
+    internal sealed class Program
     {
-        public static void Main(string[] args)
+        public static string EnvInfo => Environment.GetEnvironmentVariable("ENV_INFO");
+        
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine($"Balances service version {Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion}");
+            Console.WriteLine($"{PlatformServices.Default.Application.ApplicationName} version {PlatformServices.Default.Application.ApplicationVersion}");
 #if DEBUG
             Console.WriteLine("Is DEBUG");
 #else
             Console.WriteLine("Is RELEASE");
-#endif           
-            Console.WriteLine($"ENV_INFO: {Environment.GetEnvironmentVariable("ENV_INFO")}");
+#endif
+            Console.WriteLine($"ENV_INFO: {EnvInfo}");
 
             try
             {
-                var host = new WebHostBuilder()
+                var webHost = new WebHostBuilder()
                     .UseKestrel()
                     .UseUrls("http://*:5000")
                     .UseContentRoot(Directory.GetCurrentDirectory())
@@ -26,12 +30,25 @@ namespace Lykke.Service.Balances
                     .UseApplicationInsights()
                     .Build();
 
-                host.Run();
+                await webHost.RunAsync();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Fatal error:");
                 Console.WriteLine(ex);
+
+                // Lets devops to see startup error in console between restarts in the Kubernetes
+                var delay = TimeSpan.FromMinutes(1);
+
+                Console.WriteLine();
+                Console.WriteLine($"Process will be terminated in {delay}. Press any key to terminate immediately.");
+
+                await Task.WhenAny(
+                    Task.Delay(delay),
+                    Task.Run(() =>
+                    {
+                        Console.ReadKey(true);
+                    }));
             }
 
             Console.WriteLine("Terminated");
